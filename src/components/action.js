aff316d53f9a1ca462e55b7f6411c034453d3bd5
SaGonz <langSaGonz@gmail.com>
2019-08-19 12:16:51 +1000

    code cleanup

--- src/components/action.js
+++ src/components/action.js
@@ -1,17 +1,19 @@
 import React from 'react';
-//Images
-import yes from '../yes.svg';
-import no from '../no.svg';
 //Components
 import Lyrics from './lyrics.js';
 import Title from './title';
 //Local json in ES6
 import songs from '../songs.json';
+//Images
+import yes from '../yes.svg';
+import no from '../no.svg';
+
 
 class Action extends React.Component{
 
     constructor(props){
         super(props);
+        //These functions are binded so everytime they're invoked they're told the component is the caller
         this.getSong = this.getSong.bind(this);
         this.likedSong = this.likedSong.bind(this);
         this.dislikedSong = this.dislikedSong.bind(this);
@@ -20,38 +22,49 @@ class Action extends React.Component{
     state = {
         song: {},
         likedSongs: [],
-        dislikedSongs: []
+        dislikedSongs: [],
+        viewedSongs: [-1]
     }
 
     getRandomNumber( min, max){ return Math.floor( Math.random() * (max - min) + min);}
 
-    addLineBreak(){ return <br/>}
-
+    
     //When this method is called it pulls the song from a list with a random index,
     //and sets 'song' 's state to the response, re-rendering its children 
     //and updating their content.
     getSong () {
 
-        let r = this.getRandomNumber(0,4);
-        let randomSongIndex = r.toString();
+        let r = -1;
 
+        while(this.state.viewedSongs.includes(r)){
+           r = this.getRandomNumber(0,4);
+        }
+        
+        let randomSongIndex = r.toString();
         const song = songs[randomSongIndex];
 
-        console.log('called getsong')
-        console.log(this.state.song)
+        this.setState(
+            {
+                song: song,
+            }
+        )
 
-        this.setState({song: song})
-        console.log('after set state',this.state.song)
-
+        this.setState(
+            state => { 
+                const viewedSongList = state.viewedSongs.push(randomSongIndex);
+            }
+        );
+        
         return song;
     }
     
     likedSong(){
-        //push liked song to state
+        //Pushing liked songs to state
         this.setState(
             state => { 
                 const likedSongsList = state.likedSongs.push(this.getSong());
-            });
+            }
+        );
         
         console.log('first liked song', this.state.likedSongs[0])
     }
@@ -69,7 +82,9 @@ class Action extends React.Component{
 
         return( 
             <div>
-                <p className="titleParent"><Title titlep={this.state.song.title} artistp={this.state.song.artist}/></p>
+                <p className="titleParent">
+                    <Title titlep={this.state.song.title} artistp={this.state.song.artist}/>
+                </p>
                 <div className="lyrics-section">
                     <img id="no" src={no} className="icons" onClick={this.dislikedSong}/>
                     <Lyrics lyricsp={this.state.song.lyrics}/>
